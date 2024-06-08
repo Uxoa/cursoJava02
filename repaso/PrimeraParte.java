bdd83fcfe15b3694126a60c033e900c3505daf3e
uxoa <palomita@gmail.com>
2024-06-08 16:26:39 +0200

    Exceptions exercices

---
+++ repaso/PrimeraParte.java
@@ -0,0 +1,29 @@
+package repaso;
+
+public class PrimeraParte {
+    
+    
+    public static void main(String[] args){
+     
+     /*CREAR UN PROGRAMA QUE NOS DEVUELVA LO SIGUIENTE:
+    a. Mucho ánimo equipo.
+    b. Mi deporte favorito es descansar.
+    c. Me gusta la playa y no la montaña.
+    */
+     
+     System.out.println("Mucho ánimo equipo.");
+     System.out.println("Mi deporte favorito es descansa.r");
+     System.out.println("Me gusta la playa y no la montaña.");
+     
+     
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
